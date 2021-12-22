286448b3da96d0a871e3c52ea06ce06252f512a9
Antoine Fissot <antoine.fissot@semifir.com>
2021-12-22 09:28:58 +0100

    doc: [employees] documentation de mon test

--- src/test/java/fr/semifir/testDemo/EmployeeControllerTest.java
+++ src/test/java/fr/semifir/testDemo/EmployeeControllerTest.java
@@ -27,35 +27,85 @@ import java.util.Optional;
 
 @WebMvcTest(controllers = EmployeController.class)
 public class EmployeeControllerTest {
+
+    // On injecte MockMvc
+    // MockMvc permet de simuler un composent
     @Autowired
     private MockMvc mockMvc;
 
+    // On mock le service
+    // On récupère le service
+    // On fait une "copie" du service
     @MockBean
     private EmployeService service;
 
+    /**
+     * On va tester la route qui permet de récuperer un tableau
+     * de collaborateurs
+     * @throws Exception
+     */
     @Test
     public void testFindAllEmployees() throws Exception{
+        // On execute une request sur /employees
+        // grâce à la methode "perfom" de mockMvc
         this.mockMvc.perform(get("/employees"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$").isEmpty());
+                .andExpect(status().isOk()) // On check si le code retour et 200
+                .andExpect(jsonPath("$").isEmpty()); // On verifie si le tableau est vide
     }
 
+    /**
+     * On vérifie si la route qui permet de retrouver un collabo
+     * renvoi bien un 404 si on ne trouve pas de collabo
+     * @throws Exception
+     */
     @Test
     public void testFindOneEmployeeWhereWrongIdOrInexistantEmployee() throws Exception {
+        // On execute une request sur /employees/1
+        // grâce à la methode "perfom" de mockMvc
         this.mockMvc.perform(get("/employees/1"))
-                .andExpect(status().isNotFound());
+                .andExpect(status().isNotFound()); // On vérifie si le status est bien une 404
     }
 
+    /**
+     * On vérifie si la route qui permet de retrouver un collabo
+     * renvoi bien un collabo
+     * @throws Exception
+     */
     @Test
     public void testFindOneEmployee() throws Exception {
+        /**
+         * START
+         * On mock le service pour qu'il renvoi un employeDTO
+         * On simule l"'existance d'un employeDTO dans le BDD
+         */
+        /// On créé un employeDTO
         EmployeDTO employeDTO = this.employeDTO();
+        // On appelle la methode “given" de BDDMockito pour mocker le service
+        // On lui passe en parametre quelle méthode du service il faut mocker
         BDDMockito.given(service.findById(1L))
-                .willReturn(Optional.of(employeDTO));
+                .willReturn(Optional.of(employeDTO)); // On indique à BDDMockito : "que va tu répondre quand on appelle cette methode ?"
+        // Et on lui passe en paramètre sa réponse
+        /**
+         * END
+         */
+        /// On test notre route qui permet de récuperer un collabo
         MvcResult result = this.mockMvc.perform(get("/employees/1"))
                 .andExpect(status().isOk())
-                .andReturn();
+                .andReturn(); // On va stocker le résultat du mockMvc dans une variable
+                                // cela va nous permettre de traiter le resultat
+        // On initialise un objet Gson qui va nous permettre de manipuler/transformer
+        // des objets en JSON ou l'inverse
         Gson json = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
-        EmployeDTO body = json.fromJson(result.getResponse().getContentAsString(), EmployeDTO.class);
+        // On transform notre JSON en EmployeDTO
+        EmployeDTO body = json.fromJson(
+                // Je récupère le contenu de mon MvcResult,
+                // j'accede à la réponse : "getResponse"
+                // Je récupère le contenu du body de la réponse sous forme de String getContentAsString
+                result.getResponse().getContentAsString(),
+                // Je dis à Gson en quel objet il faut le transformer
+                EmployeDTO.class
+        );
+        // Je test
         Assertions.assertEquals(body.getUsername(), this.employeDTO().getUsername());
         Assertions.assertEquals(body.getId(), this.employeDTO().getId());
     }
@@ -87,8 +137,11 @@ public class EmployeeControllerTest {
         Gson json = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
         EmployeDTO body = json.fromJson(result.getResponse().getContentAsString(), EmployeDTO.class);
 
+        // On indique à BDDMockito :
+        // Quand on taope sur la méthode save du service avec
+        // N'importe quel objet qui est de type EmployeDTO
         BDDMockito.when(service.save(any(EmployeDTO.class)))
-                .thenReturn(employeDTOUpdated);
+                .thenReturn(employeDTOUpdated); // On retourne un employeDTOUpdated
 
         // 2 il faut le modifier
         body.setUsername("toto");
@@ -110,11 +163,12 @@ public class EmployeeControllerTest {
         Gson json = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
         String body = json.toJson(this.employeDTO());
         this.mockMvc.perform(delete("/employees")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(body))
+                        .contentType(MediaType.APPLICATION_JSON) // Le type de données que l'on passe dans notre request
+                        .content(body)) // Le contenu du body
                         .andExpect(status().isOk());
     }
 
+    // Les deux méthodes ci-dessous nous permettent de créer un employé DTO
     private EmployeDTO employeDTO() {
         return new EmployeDTO(
                 5L,
@@ -125,7 +179,6 @@ public class EmployeeControllerTest {
                 3456F
         );
     }
-
     private EmployeDTO employeDTOUpdate() {
         return new EmployeDTO(
                 5L,
